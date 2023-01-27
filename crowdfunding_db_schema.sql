aed0b25b4b848b8e31feb9320846990985698314
JenniferM-BShulyak <111457464+JenniferM-BShulyak@users.noreply.github.com>
2023-01-26 18:29:22 -0600

    Slight adjustment made to SQL file

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,4 +1,12 @@
-﻿--! Create table for contacts
+﻿--! Create database
+CREATE DATABASE crowdfunding_db
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
+
+--! Create table for contacts
 CREATE TABLE "contacts" (
     "contact_id" int   NOT NULL,
     "first_name" VARCHAR   NOT NULL,
@@ -34,9 +42,8 @@ CREATE TABLE "campaign" (
     "contact_id" INT   NOT NULL,
     "company_name" VARCHAR   NOT NULL,
     "desciption" VARCHAR   NOT NULL,
-    "goal" DOUBLE   NOT NULL,
-    "pledged" DOUBLE   NOT NULL,
-    "outcome" VARCHAR   NOT NULL,
+    "goal" FLOAT  NOT NULL,
+    "pledged" FLOAT   NOT NULL,
     "backers_count" INT   NOT NULL,
     "country" VARCHAR   NOT NULL,
     "currency" VARCHAR   NOT NULL,
@@ -58,3 +65,12 @@ REFERENCES "category" ("category_id");
 ALTER TABLE "campaign" ADD CONSTRAINT "fk_campaign_subcategory_id" FOREIGN KEY("subcategory_id")
 REFERENCES "subcategory" ("subcategory_id");
 
+
+--! Check that all tables were imported properly into the database
+SELECT * FROM campaign;
+
+SELECT * FROM category;
+
+SELECT * FROM contacts;
+
+SELECT * FROM subcategory;
