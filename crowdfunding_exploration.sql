ca14c8fd06d6d33cf358186800ad54d6dc771152
JenniferM-BShulyak <111457464+JenniferM-BShulyak@users.noreply.github.com>
2023-06-14 20:23:05 -0500

    calculated percentage successful by category

--- crowdfunding_exploration.sql
+++ crowdfunding_exploration.sql
@@ -19,10 +19,17 @@ GROUP BY company_name
 ORDER BY number_campaigns DESC
 LIMIT 10;
 
---! Most common categories
-SELECT cat.category, COUNT(cam.category_id) total
+--! Most common categories and their percentage of success
+WITH category_sum AS(
+SELECT cat.category, COUNT(cam.category_id) AS total,
+	COUNT(cam.category_id) FILTER (WHERE cam.pledged>cam.goal) AS successful,
+	COUNT(cam.category_id) FILTER (WHERE cam.pledged<cam.goal) AS unsuccessful
 FROM campaign cam
 LEFT JOIN category cat ON cam.category_id = cat.category_id
 GROUP BY cat.category
-ORDER BY total DESC;
+ORDER BY total DESC)
+
+SELECT *, ROUND((successful*100.00/total),2) AS percent_success
+FROM category_sum
+ORDER BY percent_success DESC;
 
